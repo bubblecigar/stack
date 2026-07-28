8f878c5047e59db017259a9ce1d0a582a8e02169
roy.wang <roy.wang@goanimate.com>
2026-07-29 02:04:18 +0800

    Restyle leaf math keyboard as background text

--- src/components/MathNotationPalette.js
+++ src/components/MathNotationPalette.js
@@ -62,7 +62,7 @@ export function MathNotationPalette({
             >
               {key.isSystem ? (
                 <MaterialCommunityIcons
-                  color="#64748B"
+                  color="#94A3B8"
                   name={SYSTEM_KEY_ICONS[key.systemKeyId]}
                   size={16}
                 />
